3ba829117acf1ab9496051caaadc1aaa703a73c9
Binix <shadowsouthskull@gmail.com>
2015-07-26 16:40:22 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@
 /*jshint "laxbreak":true,"shadow":true,"undef":true,"evil":true,"trailing":true,"proto":true,"withstmt":true*/
 // You may change these variables as long as you keep the same type
 var Config = {
-    base_url: "https://github.com/Binix/CaloTown-Server-Scripts",
+    base_url: "https://raw.githubusercontent.com/Binix/CaloTown-Server-Scripts/master/scripts.js",
     dataDir: "scriptdata/",
     bot: "GengarBot",
     kickbot: "VictiniBot",
